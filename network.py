e23b8e64a2e98ace3884605a8136be9d9a5e2ac5
David Smith <dasmthc@amazon.co.uk>
2020-01-06 18:34:54 +0000

    Initial work on inkyphat info display

---
+++ network.py
@@ -0,0 +1,17 @@
+import socket
+import inkyphat
+
+
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+
+font = ImageFont.truetype("./inkyphat/fonts/MesloLGS_NF_Regular.ttf", 10)
+fontsm = ImageFont.truetype("./inkyphat/fonts/MesloLGS_NF_Regular.ttf", 6)
+fontlg = ImageFont.truetype("./inkyphat/fonts/MesloLGS_NF_Regular.ttf", 16)
+
+inkyphat.set_rotation(180)
+inkyphat.set_colour('red')
+inkyphat.set_border(inkyphat.BLACK)
+
+inkyphat.text((6, 77), host_name, inkyphat.WHITE, font=font)
+inkyphat.text((6, 87), host_ip, inkyphat.WHITE, font=font)
